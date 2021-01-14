93ab942dd5c5e03f40eb6337837c66412f7baae5
ludovic <ludovic.mangaj@gmail.com>
2021-01-14 01:35:27 +0100

    update of postmanager class

--- php/model/postmanager.php
+++ php/model/postmanager.php
@@ -16,15 +16,15 @@ class PostManager
 
 	public function getAllPosts()
 	{
-		$db = $this->dbConnect(); 
-		$req = $db->query('SELECT *, post.id AS post_id, name AS author FROM post INNER JOIN user WHERE post.user_id = user.id ORDER BY creation_date DESC LIMIT 0,5'); 
+		
+		$req = $this->db->query('SELECT *, post.id AS post_id, name AS author FROM post INNER JOIN user WHERE post.user_id = user.id ORDER BY creation_date DESC LIMIT 0,5'); 
 		return $req; 
 	}
 
 	public function getPost($postId)
 	{
-		$db = $this->dbConnect(); 
-		$req = $db->prepare('SELECT post.*, user.name as author FROM post INNER JOIN user WHERE user.id = post.user_id AND post.id = ?'); 
+		
+		$req = $this->db->prepare('SELECT post.*, user.name as author FROM post INNER JOIN user WHERE user.id = post.user_id AND post.id = ?'); 
 		$req->execute(array ($postId)); 
 		$post = $req->fetch(); 
 
