de6c9125367cc9e052e29a06da50782730c3e3ea
ludovic <ludovic.mangaj@gmail.com>
2021-01-21 00:27:59 +0100

    the controller has been refactored with  and

--- php/controller/users.php
+++ php/controller/users.php
@@ -1,14 +1,16 @@
 <?php
 
 use Openclassrooms\blog\UserManager;
+use Openclassrooms\blog\User;
 
 require('model/UserManager.php');
+require('model/User.php');
 
 class Users
 {
 	public function login()
 	{
-		$UserManager = new UserManager; 
+		$userManager = new UserManager; 
 		$data = [
 			'mailError' => '',
 			'passwordError' => '', 
@@ -20,17 +22,10 @@ class Users
 
 	public function register()
 	{
-		$UserManager = new UserManager; 
-		$data = [
-			'mail' => '', 
-			'confirmMail' => '', 
-			'password' => '', 
-			'confirmPassword' => '', 
-			'mailError' => '',
-			'confirmMailError' => '',
-			'passwordError' => '', 
-			'confirmPasswordError' => '' 
-		]; 
+		$userManager = new UserManager; 
+		$user = new User; 
+
+
 
 		if($_SERVER['REQUEST_METHOD'] == 'POST') 
 		{
@@ -38,94 +33,93 @@ class Users
 			//Sanitize post data
 			$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); 
 
-			$data = [
-				'name' => trim($_POST['name']),
-				'nameError' => trim($_POST['nameError']), 
-				'lastName' => trim($_POST['lastName']), 
-				'lastNameError' => trim($_POST['lastNameError']), 
-				'mail' => trim($_POST['mail']), 
-				'confirmMail' => trim($_POST['confirmMail']), 
-				'password' => trim($_POST['password']), 
-				'confirmPassword' => trim($_POST['confirmPassword']),
-				'mailError' => trim($_POST['mailError']),
-				'confirmMailError' => trim($_POST['confirmMailError']),
-				'passwordError' => trim($_POST['passwordError']),
-				'confirmPasswordError' => trim($_POST['confirmPasswordError'])
-			]; 
+			$user->name(trim($_POST['name'])); 
+			$user->nameError(trim($_POST['nameError'])); 
+			$user->lastName(trim($_POST['lastName'])); 
+			$user->lastNameError(trim($_POST['lastNameError'])); 
+			$user->mail(trim($_POST['mail'])); 
+			$user->mailError(trim($_POST['mailError']));
+			$user->confirmMail(trim($_POST['confirmMail'])); 
+			$user->confirmMailError(trim($_POST['confirmMailError'])); 
+			$user->password(trim($_POST['password'])); 
+			$user->passwordError(trim($_POST['passwordError'])); 
+			$user->confirmPassword(trim($_POST['confirmPassword'])); 
+			$user->confirmPasswordError(trim($_POST['confirmPasswordError'])); 
+
 
 			$nameValidation = "/^[a-zA-Z0-9]*$/";
 			$passwordValidation = "/^(.{0,7}|[^a-z]*|[^\d]*)$i";   
 
 			// validate name 
 
-			if(empty($data['name'])) {
-				$data['nameError'] == 'Vous devez entrer un prénom'; 
-			} elseif (!preg_match($nameValidation, $data['name'])) {
-				$data['nameError'] == 'les prénoms ne peuvent contenir que des lettres et des chiffres'; 
+			if(empty($user->name())) {
+				$user->setNameError('Vous devez entrer un prénom'); 
+			} elseif (!preg_match($nameValidation, $user->name())) {
+				$user->setNameError('les prénoms ne peuvent contenir que des lettres et des chiffres'); 
 			} 
 
 			// validate last name
 
-			if(empty($data['lastName'])) {
-				$data['lastNameError'] == 'Vous devez entrer un nom'; 
-			} elseif (!preg_match($nameValidation, $data['lastName'])) {
-				$data['lastNameError'] == 'les snoms ne peuvent contenir que des lettres et des chiffres'; 
+			if(empty($user->lastName())) {
+				$user->setLastNameError('Vous devez entrer un nom'); 
+			} elseif (!preg_match($nameValidation, $user->lastName())) {
+				$user->setLastNameError('les noms ne peuvent contenir que des lettres et des chiffres'); 
 			} 
 
 			 // validate email
 
-			if(empty($data['mail'])) {
-				$data['mailError'] == 'Vous devez entrer une adresse e-mail'; 
-			} elseif (!filter_var($data['mail'], FILTER_VALIDATE_EMAIL)) {
-				$data['mailError'] == 'l\'adresse e-mail n\'est pas valide'; 
+			if(empty($user->mail())) {
+				$user->setMailError('Vous devez entrer une adresse e-mail'); 
+			} elseif (!filter_var($user->mail(), FILTER_VALIDATE_EMAIL)) {
+				$user->setMailError('l\'adresse e-mail n\'est pas valide'); 
 			} else {
 				// check if e-mail exists 
-				if($UserManager->findUserByMail($data['mail'])) {
-					$data['mailError'] == 'un compte avec cette adresse e-mail existe déjà'; 
+				if($userManager->findUserByMail($user->mail())) {
+					$user->setMailError('un compte avec cette adresse e-mail existe déjà'); 
 				}
 			}
 
 			//validate confirm mail 
 
-			if(empty($data['confirMail'])) {
-				$data['confirmMailError'] == 'Vous devez entrer une adresse e-mail'; 
+			if(empty($user->confirmMail())) {
+				$user->setConfirmMailError('Vous devez entrer une adresse e-mail'); 
 			} else {
-				if($data['mail'] != $data['confirmMail']) {
-					$data['confirmMailError'] == 'les adresses e-mail ne correspondent pas'; 
+				if($user->mail() != $user->confirmMail()) {
+					$user->setConfirmMailError('les adresses e-mail ne correspondent pas'); 
 				}
 			}
 
 
 			//validate password on length and numeric values
 
-			if(empty($data['password'])) {
-				$data['passwordError'] == 'Vous devez entrer un mot de passe'; 
-			} elseif (strlen($data['password']) < 8) {
-				$data['passwordError'] == 'le mot de passe doit faire au moins 8 caractères'; 
-			} elseif (!preg_match($passwordValidation, $data['password'])) {
-				$data['passwordError'] == 'Les mots de passe doivent contenir au moins un numéro'; 
+			if(empty($user->password())) {
+				$user->setPasswordError('Vous devez entrer un mot de passe'); 
+			} elseif (strlen($user->password()) < 8) {
+				$user->setPasswordError('le mot de passe doit faire au moins 8 caractères'); 
+			} elseif (!preg_match($passwordValidation, $user->password())) {
+				$user->setPasswordError('Les mots de passe doivent contenir au moins un numéro'); 
 			} 
 
 			//validate confirm password
 
-			if(empty($data['confirmPassword'])) {
-				$data['confirmPasswordError'] == 'Vous devez confirmer votre mot de passe'; 
+			if(empty($user->confirmPassword())) {
+				$user->setConfirmPasswordError('Vous devez confirmer votre mot de passe'); 
 			} else {
-				if($data['password'] != $data['confirmPassword']) {
-					$data['confirmPasswordError'] == 'les mots de passe ne correspondent pas'; 
+				if($user->password() != $user->confirmPassword()) {
+					$user->setConfirmPasswordError('les mots de passe ne correspondent pas'); 
 				}
 			}
 
 			//make sure that errors are empty
 
-			if(empty($data['nameError']) && empty($data['lastNameError']) && empty($data['mailError']) && empty($data['cofirmMailError']) && empty($data['passwordError']) && empty($data['confirmPasswordError'])) {
+			if(empty($user->nameError()) && empty($user->lastNameError()) && empty($user->mailError()) && empty($user->confirmMailError()) && empty($user->passwordError()) && empty($user->confirmPasswordError())) {
 
 				// Hash password
-				$data['password'] = password_hash($data['password'], PASSWORD_DEFAULT); 
+				$user->setPassword(password_hash($data['password'], PASSWORD_DEFAULT)); 
 
 				// Register user from model function 
 
-				if ($UserManager->register($data)){
+				if ($userManager->register($user)){
 					// redirect to the login page
 					header('location: index.php?action=login'); 
 				} else {
